ffb4b09c329775f3fa4d93ae9ecdbe74191e1dc2
kladpiban112 <57986918+kladpiban112@users.noreply.github.com>
2020-01-25 16:03:04 +0700

    login

--- views/Login/Login.js
+++ views/Login/Login.js
@@ -97,7 +97,7 @@ export class Login extends React.Component {
         }
 
         return (
-            <ScrollView style={{ backgroundColor: "#010001", }}>
+            <ScrollView style={{ backgroundColor: "skyblue", }}>
                 <StatusBar hidden={true} />
                 <View style={{ padding: 36, }}>
                     <Image 
